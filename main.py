e37157cecfbf064116ba21a94f027b8414fb37f1
Reo-Satooooo <cist.b219.r.satou@gmail.com>
2023-11-15 18:11:55 +0900

    起動設定を変更

--- main.py
+++ main.py
@@ -27,6 +27,3 @@ async def read_sheet():
   pdfM.get_pdf_from_web()
   json = jsonM.new_json()
   return json
-
-if __name__ == "__main__":
-  uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
